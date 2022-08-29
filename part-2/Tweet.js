a74df518932465d7bba0c1e2fd5a740ef4b001f1
Ryan Rustyn <ryanrustyn@gmail.com>
2022-08-29 12:15:30 -0700

    added style

--- part-2/Tweet.js
+++ part-2/Tweet.js
@@ -1,8 +1,13 @@
 "use strict";
 
 function Tweet({username, name, date, msg}) {
+    const myStyle = {
+        border: "1px black solid",
+        backgroundColor: "blue",
+    }
+    
     return(
-        <div>
+        <div style={myStyle}>
             <div>{username}</div>
             <div>{name} {date}</div>
             <div>{msg}</div>
